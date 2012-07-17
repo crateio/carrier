4086586b832e757108e1abd9037b0cb033bf6948
Donald Stufft <donald.stufft@gmail.com>
2012-07-17 01:12:21 -0400

    Add a store backend for storing data

---
+++ conveyor/store.py
@@ -0,0 +1,21 @@
+class BaseStore(object):
+
+    def set(self, key, value):
+        raise NotImplementedError
+
+    def get(self, key):
+        raise NotImplementedError
+
+
+class InMemoryStore(object):
+
+    def __init__(self, *args, **kwargs):
+        super(InMemoryStore, self).__init__(*args, **kwargs)
+
+        self._data = {}
+
+    def set(self, key, value):
+        self._data[key] = value
+
+    def get(self, key):
+        return self._data[key]
