f53bd64d98eed03a986882cab575d0172ed36dcf
Donald Stufft <donald.stufft@gmail.com>
2012-07-17 00:55:16 -0400

    Serialize files from PyPI and upload them to Warehouse

--- conveyor/processor.py
+++ conveyor/processor.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 
+import base64
 import collections
 import re
 
@@ -122,8 +123,21 @@ class BaseProcessor(object):
             # @@@ Update Version
             pass
 
-        # @@@ Get/Create/Delete Files
-        # @@@ Batch Update?
+        for f in release["files"]:
+            try:
+                vfile = self.warehouse.projects(release["normalized"]).versions(release["version"]).files(f["filename"]).get()
+            except slumber.exceptions.HttpClientError as e:
+                if not e.response.status_code == 404:
+                    raise
+                try:
+                    data = self.to_warehouse_file(release, f, extra={"version": version["resource_uri"]})
+                    vfile = self.warehouse.projects(release["normalized"]).versions(release["version"]).files.post(data)
+                except Exception as e:
+                    print e.response.content
+                    raise
+            else:
+                # @@@ Update File
+                pass
 
     def to_warehouse_project(self, release, extra=None):
         data = {"name": release["name"]}
@@ -201,8 +215,19 @@ class BaseProcessor(object):
 
         return data
 
-    def to_warehouse_file(self, release, extra=None):
-        pass
+    def to_warehouse_file(self, release, file, extra=None):
+        data = {
+            "file": {"name": file["filename"], "file": base64.b64encode(file["file_data"])},
+            "created": file["upload_time"].isoformat(),
+            "type": file["packagetype"],
+            "python_version": file["python_version"],
+            "comment": file["comment_text"],
+        }
+
+        if extra is not None:
+            data.update(extra)
+
+        return data
 
 
 class BulkProcessor(BaseProcessor):
