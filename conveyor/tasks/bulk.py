898a2c8ec07f2aa5e4cbe84cb75b2e1a2b56781f
Donald Stufft <donald.stufft@gmail.com>
2012-08-05 15:16:47 -0400

    Log exceptions that occur in the bulk processing

--- conveyor/tasks/bulk.py
+++ conveyor/tasks/bulk.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import time
 
 import slumber
@@ -6,53 +7,63 @@ import slumber
 from conveyor.core import Conveyor
 from conveyor.processor import get_key
 
+logger = logging.getLogger(__name__)
+
 
 # We ignore the last component as we cannot properly handle it
 def get_jobs(last=0):
-    current = time.mktime(datetime.datetime.utcnow().timetuple())
+    try:
+        current = time.mktime(datetime.datetime.utcnow().timetuple())
 
-    app = Conveyor()
+        app = Conveyor()
 
-    warehouse = slumber.API(
-                        app.config["conveyor"]["warehouse"]["url"],
-                        auth=(
-                            app.config["conveyor"]["warehouse"]["auth"]["username"],
-                            app.config["conveyor"]["warehouse"]["auth"]["password"],
+        warehouse = slumber.API(
+                            app.config["conveyor"]["warehouse"]["url"],
+                            auth=(
+                                app.config["conveyor"]["warehouse"]["auth"]["username"],
+                                app.config["conveyor"]["warehouse"]["auth"]["password"],
+                            )
                         )
+        processor_class = app.get_processor_class()
+        processor = processor_class(
+                        index=app.config["conveyor"]["index"],
+                        warehouse=warehouse,
+                        store=app.redis,
+                        store_prefix=app.config.get("redis", {}).get("prefix", None)
                     )
-    processor_class = app.get_processor_class()
-    processor = processor_class(
-                    index=app.config["conveyor"]["index"],
-                    warehouse=warehouse,
-                    store=app.redis,
-                    store_prefix=app.config.get("redis", {}).get("prefix", None)
-                )
 
-    names = set(processor.client.list_packages())
+        names = set(processor.client.list_packages())
 
-    for package in names:
-        yield package
+        for package in names:
+            yield package
 
-    processor.store.set(get_key(processor.store_prefix, "pypi:since"), current)
+        processor.store.set(get_key(processor.store_prefix, "pypi:since"), current)
+    except Exception as e:
+        logger.exception(str(e))
+        raise
 
 
 def handle_job(name):
-    app = Conveyor()
+    try:
+        app = Conveyor()
 
-    warehouse = slumber.API(
-                        app.config["conveyor"]["warehouse"]["url"],
-                        auth=(
-                            app.config["conveyor"]["warehouse"]["auth"]["username"],
-                            app.config["conveyor"]["warehouse"]["auth"]["password"],
+        warehouse = slumber.API(
+                            app.config["conveyor"]["warehouse"]["url"],
+                            auth=(
+                                app.config["conveyor"]["warehouse"]["auth"]["username"],
+                                app.config["conveyor"]["warehouse"]["auth"]["password"],
+                            )
                         )
+        processor_class = app.get_processor_class()
+        processor = processor_class(
+                        index=app.config["conveyor"]["index"],
+                        warehouse=warehouse,
+                        store=app.redis,
+                        store_prefix=app.config.get("redis", {}).get("prefix", None)
                     )
-    processor_class = app.get_processor_class()
-    processor = processor_class(
-                    index=app.config["conveyor"]["index"],
-                    warehouse=warehouse,
-                    store=app.redis,
-                    store_prefix=app.config.get("redis", {}).get("prefix", None)
-                )
 
-    for release in processor.get_releases(name):
-        processor.sync_release(release)
+        for release in processor.get_releases(name):
+            processor.sync_release(release)
+    except Exception as e:
+        logger.exception(str(e))
+        raise
