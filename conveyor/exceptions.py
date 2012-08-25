e85883389dd14377d63fc8c0b4decf486b3b7c2c
Donald Stufft <donald.stufft@gmail.com>
2012-08-25 19:43:26 -0400

    Bring the standard imports over

--- conveyor/exceptions.py
+++ conveyor/exceptions.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+
 class HashMismatch(ValueError):
     """
     Raised when the incoming hash of a file does not match the expected.
